b998715fd1ffec0875bc87fa112281563e76e7a6
Rafael <rafaelblima22@gmail.com>
2026-09-16 21:02:13 -0300

    Aula do dia 16/09/2026

--- Parte07-Flask/04_poltergit/app.py
+++ Parte07-Flask/04_poltergit/app.py
@@ -1,10 +1,34 @@
 from flask import Flask, render_template, request
-
+from datetime import date
+import pyautogui as auto
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/commitar", methods = ['POST'])
+def commitar():
+    hoje = date.today().strftime("%d/%m/%Y")
+    repositorio = None
+    if request.method == "POST":
+        repositorio = request.form.get("repositorio","")
+    if repositorio:
+        auto.PAUSE = 1
+        auto.hotkey("win","r")
+        auto.write("cmd")
+        auto.press("enter")
+        auto.write(f"cd {repositorio}")
+        auto.press("enter")
+        auto.write("git add .")
+        auto.press("enter")
+        auto.write(f'git commit -m "Aula do dia {hoje}"')
+        auto.press("enter")
+        auto.write("git push")
+        auto.sleep(3)
+        auto.write("exit")
+        auto.press("enter")
     return render_template("index.html")
 
 
